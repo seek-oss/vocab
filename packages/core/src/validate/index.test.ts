41fe39c2c2b65dfa6bfa0aee2f08d4be60e6eaf9
Matt Jones <mattjones701@gmail.com>
2020-12-11 10:22:47 +1100

    Fix validator tests

--- packages/core/src/validate/index.test.ts
+++ packages/core/src/validate/index.test.ts
@@ -31,11 +31,10 @@ const testCases: Array<TestCase> = [
       relativePath: 'some-file.json',
       namespace: 'some-file-2',
       keys: ['key1'],
-      // @ts-expect-error
-      languages: new Map([
-        ['en', { key1: { message: 'Hi' } }],
-        ['th', {}],
-      ]),
+      languages: {
+        en: { key1: { message: 'Hi' } },
+        th: {},
+      },
     },
     devLanguage: 'en',
     altLanguages: ['th'],
