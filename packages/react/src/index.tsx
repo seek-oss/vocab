390c2cc277940f67a2ab6aed83e23525cba88dbd
Matt Jones <mattjones701@gmail.com>
2020-12-15 08:29:43 +1100

    Formatting

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -70,7 +70,9 @@ export function useTranslation<Translations extends BaseTranslation>(
 
   if (!translationsObject) {
     if (SERVER_RENDERING) {
-      throw new Error(`Translations not syncronously available on server render. This should not happen.s`);
+      throw new Error(
+        `Translations not syncronously available on server render. This should not happen.`,
+      );
     }
     translations[language].load().then(() => {
       translationsObject = translations[language].getValue();
