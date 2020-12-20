14e9acd14ee95af1174690550fbcd3d3e29c1e88
Matt Jones <mattjones701@gmail.com>
2020-12-21 10:33:15 +1100

    Import order

--- packages/core/src/validate/index.ts
+++ packages/core/src/validate/index.ts
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
 import { UserConfig, LoadedTranslation, LanguageName } from '@vocab/types';
 import chalk from 'chalk';
-import { loadAllTranslations } from '../load-translations';
 
+import { loadAllTranslations } from '../load-translations';
 import { getAltLanguages } from '../utils';
 
 export function findMissingKeys(
