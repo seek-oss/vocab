a468444ad7beb2181cdf4b64ed7a714198072d47
Jahred Hope <jahredhope@gmail.com>
2021-01-27 11:05:32 +1100

    Use sarisia icu actions

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,3 @@
 import 'expect-puppeteer';
 
-// Polyfil intl-locale to ensure full ICU has been loaded
-// https://nodejs.org/api/intl.html#intl_options_for_building_node_js
-import '@formatjs/intl-locale/polyfill';
-
 jest.setTimeout(20000);
--- packages/core/src/runtime.test.ts
+++ packages/core/src/runtime.test.ts
@@ -1,7 +1,3 @@
-// Polyfil intl-locale to ensure full ICU has been loaded
-// https://nodejs.org/api/intl.html#intl_options_for_building_node_js
-import '@formatjs/intl-locale/polyfill';
-
 import { createTranslationFile, createLanguage } from './runtime';
 
 const createDemoTranslationFile = () =>
