37513013d869d6813c20d9ae9d53ac1fe2be2629
mattcompiles <mattjones701@gmail.com>
2021-09-01 13:19:19 +1000

    Remove compiler target warning (#68)

--- packages/webpack/src/index.ts
+++ packages/webpack/src/index.ts
@@ -28,16 +28,6 @@ export class VocabWebpackPlugin {
     trace(
       `Applying plugin: ${compiler.options.name} (${compiler.options.target})`,
     );
-    if (compiler.options.target && compiler.options.target !== 'web') {
-      // eslint-disable-next-line no-console
-      console.error(
-        `Vocab plugin is only intended to be used on web builds. Received target ${
-          compiler.options.target
-        } for ${
-          compiler.options.name || 'unnamed build'
-        }. Did you add Vocab to the correct config?`,
-      );
-    }
     if (!compiler.options.module) {
       // @ts-expect-error Support for older versions of webpack that may not have module defined at this stage
       compiler.options.module = { rules: [] };
