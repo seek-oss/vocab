6bf7040dd3e560944708d25c91b2010e8748cf5f
William Lark <wlark@seek.com.au>
2025-08-04 17:16:50 +1000

    adding vitest rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,23 @@
-import eslintConfigSeek from 'eslint-config-seek';
+/* eslint-disable import-x/no-rename-default */
+import seek from 'eslint-config-seek';
+import vitest from '@vitest/eslint-plugin';
+
+const modifiedSeek = seek.map((config) => {
+  // Removing the jest plugin and rules so they don't conflict with the vitest plugin
+  if (config.plugins?.jest) {
+    delete config.plugins.jest;
+  }
+
+  if (config.rules) {
+    for (const ruleName of Object.keys(config.rules)) {
+      if (ruleName.includes('jest')) {
+        config.rules[ruleName] = 'off';
+      }
+    }
+  }
+
+  return config;
+});
 
 export default [
   {
@@ -13,13 +32,22 @@ export default [
       '**/*.vocab/index.ts',
     ],
   },
-  ...eslintConfigSeek,
+  ...modifiedSeek,
   {
     rules: {
       'no-process-exit': 'off',
       'no-var-requires': 'off',
       '@typescript-eslint/no-var-requires': 'off',
       '@typescript-eslint/consistent-type-definitions': 'off',
+    },
+  },
+  {
+    plugins: {
+      vitest,
+    },
+    rules: {
+      ...vitest.configs.recommended.rules,
+      'vitest/no-focused-tests': 'error',
     },
   },
 ];
